e14b172fd7b49ff36d8058b7ecc2967477afd411
katai izano <515haidarlathif.ash@gmail.com>
2026-03-21 10:36:11 +0700

    menambahkan segmen fitur yang ingin diimplementasikan

---
+++ Script.py
@@ -0,0 +1,25 @@
+import os
+
+def direction():
+    pass
+
+def changeDirection(req):
+    pass
+
+def createFolder():
+    pass
+
+def moveFolder():
+    pass
+
+def deleteFolder():
+    pass
+
+def renameFolder():
+    pass
+
+def printHelp():
+    pass
+
+def main():
+    print()
