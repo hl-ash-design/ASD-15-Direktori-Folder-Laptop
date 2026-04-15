2a2e029f1dd88c6250fb879d9f0d473c0dcf8fec
katai izano <515haidarlathif.ash@gmail.com>
2026-04-15 12:57:03 +0700

    menambahkan command

--- Script.py
+++ Script.py
@@ -26,4 +26,27 @@ def printHelp():
     pass
 
 def main():
-    print()
+    printHelp()
+    command = input("Enter a command: ")
+    while command != "exit":
+        if command == "dir":
+            directory()
+        elif command == "cd":
+            changeDirectory()
+        elif command == "mkdir":
+            createFolder()
+        elif command == "move":
+            moveFolder()
+        elif command == "rmdir":
+            deleteFolder()
+        elif command == "rename":
+            renameFolder()
+        elif command == "search":
+            searchDirectory()
+        elif command == "help":
+            printHelp()
+        else:
+            print("Invalid command. Type 'help' for a list of commands.")
+        
+        command = input("Enter a command: ")
+    print("Exiting...")
